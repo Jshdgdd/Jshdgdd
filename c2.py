4a24f8e8ada6a49fd1953104b0bea8e0bfbe31a6
Ntllllll <106831254+Jshdgdd@users.noreply.github.com>
2022-10-18 21:45:46 +0700

    Update c2.py

--- c2.py
+++ c2.py
@@ -1,5 +1,5 @@
 ##############################
-# ZxCDDoS Made by zxcr9999   #
+# ZxCDDoS Made by Zxr9999   #
 # COPY = NIGGER              #
 ##############################
 
@@ -226,7 +226,7 @@ def menu():
                 \x1b[38;2;0;212;14m║ \x1b[38;2;0;49;147m- - - - - - \x1b[38;2;239;239;239mFree DDoS Panel 2022\x1b[38;2;0;212;14m- - - - - - -\x1b[38;2;0;49;147m║
                 \x1b[38;2;0;212;14m╚═══════════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════════╝
                     \x1b[38;2;0;212;14m╔═══════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════╗
-                    \x1b[38;2;0;212;14m║ \x1b[38;2;239;239;239mhttps://github.com/hoaan1995/ZxCDDoS \x1b[38;2;0;49;147m║
+                    \x1b[38;2;0;212;14m║ \x1b[38;2;239;239;239m \x1b[38;2;0;49;147m║
                     \x1b[38;2;0;212;14m╚═══════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════╝
                 \x1b[38;2;0;212;14m╔═══════════\x1b[38;2;0;186;45m════════\x1b[38;2;0;150;88m═══════\x1b[38;2;0;113;133m═════\x1b[38;2;0;83;168m═════\x1b[38;2;0;49;147m══════════╗
                 \x1b[38;2;0;212;14m║   \x1b[38;2;239;239;239m   Type help to see the all commands.      \x1b[38;2;0;49;147m║
@@ -755,7 +755,25 @@ CLEAR   ► CLEAR TERMINAL
 def login():
     clear()
     user = "admin"
-    passwd = "admin"
+    passwd = "thanhlam123"
+    username = input("⚡ Username: ")
+    password = getpass.getpass(prompt='⚡ Password: ')
+    if username != user or password != passwd:
+        print("")
+        print("⚡ Haizzz, you're so cute...")
+        sys.exit(1)
+    elif username == user and password == passwd:
+        print("⚡ Welcome to ZxC C2!")
+        time.sleep(0.3)
+        ascii_vro()
+        main()
+
+login()
+
+def login():
+    clear()
+    user = "ntl"
+    passwd = "123"
     username = input("⚡ Username: ")
     password = getpass.getpass(prompt='⚡ Password: ')
     if username != user or password != passwd:
